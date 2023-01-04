90408b8581de55110ee805ab8f7a025bff9f68e9
Megha Sahu <59828403+megha9876@users.noreply.github.com>
2023-01-04 21:01:41 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -6,25 +6,22 @@ import line_ap from "./assets/line.png"
 import "./components/Navbar.css"
 function App() {
 return (
-	<div className="App">	
-  <div className="fixed-nav">
-      <div className='logo'>
-          <img src={logo_ap} alt = "ap-logo"></img>
+	<div className="App">	          
+    <div className='topnav'>
+      <div className="text-on-image " > 
+        <ul>       
+          <li><AnchorLink href='#home'>HOME</AnchorLink></li>
+          <li><AnchorLink href='#products'>PRODUCTS</AnchorLink></li>
+          <li><AnchorLink href='#global'>GLOBAL PRESENCE</AnchorLink></li>
+          <li><AnchorLink href='#about'>ABOUT</AnchorLink></li>
+          <li><AnchorLink href='#contact'>CONTACT US</AnchorLink></li> 
+        </ul> 
+        <div className="logo"> 
+          <img src={logo_ap} alt="logoimg" ></img>
+        </div>       
       </div>
-      <div className ="head-text">
-          <img src={line_ap} alt = "ap-line"></img>              
-          <div className='text-on-image'>
-            <div className="topnav">               
-              <AnchorLink href='#home'>HOME</AnchorLink>
-              <AnchorLink href='#products'>PRODUCTS</AnchorLink>
-              <AnchorLink href='#global'>GLOBAL PRESENCE</AnchorLink>
-              <AnchorLink href='#about'>ABOUT</AnchorLink>
-              <AnchorLink href='#contact'>CONTACT US</AnchorLink>
-            </div>
-        </div>
-  </div>
-  </div>
-	<Navbar/>
+    </div>
+	  <Navbar/>
 	</div>
 );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,154 +1,56 @@
 import React from 'react'
 import homeImage from "../assets/young.jpg"
 import globalImage from "../assets/global.jpg"
+import tablet from "../assets/ExcludeB.png"
+import ProductItem, { item } from "./productItem"
 
 const Home = () => {
 	return (
-		<div id="home">
+		<div id="home" className='scrolltext'>
 			<div className="homePage">
-                <img src={homeImage} alt = "Home-Image"></img> 
+                <img src={homeImage} alt = "Home-Images"></img> 
             </div>
 		</div>
 	)
 }
 
+const Products1 = () => {
+    let arr = [];
+
+    for (let key of item)
+    arr.push(<li>{key}</li>)
+
+    return(
+        <div>{arr}</div>
+        
+    )
+}
+
+
 const Products = () => {
 	return (
-		<div id="products">
+		<section id="products" className='scrolltext'>
 			<div className='about_c'>
                 <div className='side-name'>
                     <h2>PRODUCTS LIST</h2>
                 </div>
-                <div className='list'>
-                    
-                <p>	S. NO.	PRODUCTS	</p>
-                <p>	1.		2-DEOXY-D-GLUCOSE	</p>
-                <p>	2.		ABIRATERONE ACETATE	</p>
-                <p>	3.		AMLODIPINE USP/IP/BP	</p>
-                <p>	4.		ATRACURIUM BESYLATE EP	</p>
-                <p>	5.		BALOXAVIR MARBOXIL	</p>
-                <p>	6.		BECLOMETASONE DIPROPIONATE	</p>
-                <p>	7.		BEMPEDOIC ACID	</p>
-                <p>	8.		BENFOTIAMINE IH	</p>
-                <p>	9.		BETAMETHASONE	</p>
-                <p>	10.		BETAMETHASONE ACETATE	</p>
-                <p>	11.		BETAMETHASONE DIPROPIONATE	</p>
-                <p>	12.		BETAMETHASONE SODIUM PHOSPHATE	</p>
-                <p>	13.		BETAMETHASONE VALERATE	</p>
-                <p>	14.		BILASTINE	</p>
-                <p>	15.		BREXPIPRAZOLE	</p>
-                <p>	16.		BREXPIPRAZOLE IH	</p>
-                <p>	17.		BUDESONIDE	</p>
-                <p>	18.		CLASCOTERONE	</p>
-                <p>	19.		CLOBETASOL PROPIONATE	</p>
-                <p>	20.		CLOBETASONE BUTYRATE	</p>
-                <p>	21.		CLOCORTOLONE PIVALATE	</p>
-                <p>	22.		CLOPIDOGREL USP/IP	</p>
-                <p>	23.		CLOPREDNOL	</p>
-                <p>	24.		CRISABOROLE	</p>
-                <p>	25.		DACLATASVIR	</p>
-                <p>	26.		DAPAGLIFLOZIN IH	</p>
-                <p>	27.		DAPAGLIFLOZIN MONO HYDRADE	</p>
-                <p>	28.		DEFERASIROX EP/IH	</p>
-                <p>	29.		DEFLAZACORT	</p>
-                <p>	30.		DESONIDE	</p>
-                <p>	31.		DESOXIMETHASONE	</p>
-                <p>	32.		DEXAMETHASONE	</p>
-                <p>	33.		DEXAMETHASONE SODIUM PHOSPHATE	</p>
-                <p>	34.		DEXKETOPROFEN TROMETAMOL	</p>
-                <p>	35.		DOBUTAMINE HCl	</p>
-                <p>	36.		DIFLUPREDNATE	</p>
-                <p>	37.		EFINACONAZOLE	</p>
-                <p>	38.		ELOBIXIBAT	</p>
-                <p>	39.		EMPAGLIFLOZIN	</p>
-                <p>	40.		EMPAGLIFLOZIN IH	</p>
-                <p>	41.		ENTECAVIR	</p>
-                <p>	42.		ES CITALOPRAM OXALATE	</p>
-                <p>	43.		ETORICOXIB	</p>
-                <p>	44.		ETORICOXIB EP/USP/IP/BP	</p>
-                <p>	45.		FAVIPIRAVIR 	</p>
-                <p>	46.		FENTICONAZOLE NITRATE EP	</p>
-                <p>	47.		FERRIC MALTOL	</p>
-                <p>	48.		FEXOFENADINE HCL	</p>
-                <p>	49.		FLUCONAZOLE	</p>
-                <p>	50.		FLUMETHASONE	</p>
-                <p>	51.		FLUOCINOLONE ACETONIDE	</p>
-                <p>	52.		FLUOROMETHOLONE	</p>
-                <p>	53.		FLURANDRENOLIDE	</p>
-                <p>	54.		FLURBIPROFEN EP/USP/IP/BP	</p>
-                <p>	55.		FLUTICASONE FUROATE	</p>
-                <p>	56.		FLUTICASONE PROPIONATE	</p>
-                <p>	57.		HALOBETASOL PROPIONATE	</p>
-                <p>	58.		HALOMETASONE MONOHYDRATE	</p>
-                <p>	59.		HYDROCORTISONE	</p>
-                <p>	60.		HYDROCORTISONE ACETATE	</p>
-                <p>	61.		HYDROCORTISONE BUTYRATE	</p>
-                <p>	62.		HYDROCORTISONE HEMISUCCINATE	</p>
-                <p>	63.		HYDROCORTISONE SODIUM SUCCINATE (BUFFERED 5%)	</p>
-                <p>	64.		HYDROCORTISONE VALERATE	</p>
-                <p>	65.		IMEGLIMIN	</p>
-                <p>	66.		ISAVUCONAZONIUM SULPHATE	</p>
-                <p>	67.		ISOSORBIDE MONOITRATE	</p>
-                <p>	68.		KETOPROFEN	</p>
-                <p>	69.		KETOPROFEN IP/EP/USP	</p>
-                <p>	70.		LEVOBUPIVACAINE HCl	</p>
-                <p>	71.		LURASIDONE HYDROCHLORIDE IH	</p>
-                <p>	72.		MEROPENEM	</p>
-                <p>	73.		METHYL PREDNISOLONE	</p>
-                <p>	74.		METHYLPREDNISOLONE ACEPONATE	</p>
-                <p>	75.		METHYLPREDNISOLONE ACETATE	</p>
-                <p>	76.		METHYLPREDNISOLONE HEMISUCCINATE	</p>
-                <p>	77.		METHYLPREDNISOLONE SODIUM SUCCINATE (BUFFERED 3%)	</p>
-                <p>	78.		METOPROLOL SUCCINATE EP/USP/IP	</p>
-                <p>	79.		METOPROLOL TARTRATE EP/USP/IP	</p>
-                <p>	80.		MOLNUPIRAVIR	</p>
-                <p>	81.		MOMETASONE FUROATE	</p>
-                <p>	82.		MOMETASONE FUROATE MONOHYDRATE	</p>
-                <p>	83.		NIMODIPINE BP	</p>
-                <p>	84.		NIRMATRELVIR	</p>
-                <p>	85.		OBETICHOLIC ACID	</p>
-                <p>	86.		PANTOPRAZOLE IP/BP	</p>
-                <p>	87.		PLECANATIDE	</p>
-                <p>	88.		PREDNISOLONE	</p>
-                <p>	89.		PREDNISOLONE ACETATE	</p>
-                <p>	90.		PREDNISOLONE HEMISUCCINATE	</p>
-                <p>	91.		PREDNISOLONE SODIUM PHOSPHATE	</p>
-                <p>	92.		PREDNISONE	</p>
-                <p>	93.		PREGABALIN	</p>
-                <p>	94.		PROPOFOL BP	</p>
-                <p>	95.		ROCURONIUM BROMIDE	</p>
-                <p>	96.		ROSUVASTATIN CALCIUM EP/USP/IP	</p>
-                <p>	97.		ROXADUSTAT	</p>
-                <p>	98.		SARECYCLINE	</p>
-                <p>	99.		SERTACONAZOLE NITRATE	</p>
-                <p>	100.		SITAGLIPTIN PHOSPHATE	</p>
-                <p>	101.		SOFOSBUVIR	</p>
-                <p>	102.		TACROLIMUS	</p>
-                <p>	103.		TAMSULOSIN HCL EP/USP/IP/BP/JP	</p>
-                <p>	104.		TAVABOROLE	</p>
-                <p>	105.		TEDIZOLID PHOSPHATE	</p>
-                <p>	106.		TENELIGLIPTIN HBR HYDRATE	</p>
-                <p>	107.		TRAMADOL HCL	</p>
-                <p>	108.		TRIAMCINOLONE ACETONIDE	</p>
-                <p>	109.		TRICLABENDAZOLE EP/USP/IP/BP	</p>
-                <p>	110.		TRICLOFOS SODIUM 	</p>
-                <p>	111.		VILDAGLIPTIN 	</p>
-
+                <div className='productName'>
+                    {Products1()}
                 </div>
             </div>
-		</div>
+		</section>
 	)
 }
 
 const Global = () => {
 	return (
-		<div id="global">
+		<div id="global" className='scrolltext'>
 			<div className='about_c'>
                 <div className='side-name'>
                     <h2>Global Presence</h2>
                 </div>
                 <div className='global-img'>
-                    <img src={globalImage} alt = "Home-Image"></img> 
+                    <img src={globalImage} alt = "Home-Images"></img> 
                 </div>
             </div>
 		</div>
@@ -157,31 +59,34 @@ const Global = () => {
 
 const About = () => {
 	return (
-		<div id="about">
+		<section id="about" className='scrolltext'>
 			<div className='about_c'>
                 <div className='side-name'>
                     <h2>ABOUT</h2>
                 </div>
                 <div className='content'>
                     <p>
-                    We, Apoteca Pharma Private Limited is an established pharmaceutical trade company whose main activity is to export pharmaceutical products. We aim to build a strong bridge between manufacturer and the formulator to minimize the gap.
-                    With experience and insight of the Industry we are able to fulfill the industry’s needs. We as a company are specialized in providing pharmaceutical APIs and Excipients. We provide a wide range of products throughout the world. Many Indian manufacturers are closely associated with us for API and Excipients supplies.
+                        We, Apoteca Pharma Private Limited is an established pharmaceutical trade company whose main activity is to export pharmaceutical products. We aim to build a strong bridge between manufacturer and the formulator to minimize the gap.
+                    </p>
+                    <br></br>
+                    <p>
+                        With experience and insight of the Industry we are able to fulfill the industry’s needs. We as a company are specialized in providing pharmaceutical APIs and Excipients. We provide a wide range of products throughout the world. Many Indian manufacturers are closely associated with us for API and Excipients supplies.
                     </p>
                 </div>
             </div>
-		</div>
+		</section>
 	)
 }
 
 const Contact = () => {
 	return (
-		<div id="contact">
+		<section id="contact" className='scrolltext'>
 			<div className='about_c'>
                 <div className='side-name'>
                     <h2>CONTACT</h2>
                 </div>
 
-                <div className='content'>
+                <div className='content1'>
                     <h2>Apoteca Pharma Private Limited</h2>
 
                     <h3>Office : </h3>
@@ -195,7 +100,7 @@ const Contact = () => {
                     <h4>GST No.: 23AAWCA8302L1ZC</h4>
                 </div>
             </div>
-		</div>
+		</section>
 	)
 }
 
